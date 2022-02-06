f22b67d7bcfdb05f7f9cb7c2fe82950f94801d3a
andrew4779 <andrew.kimani@student.moringaschool.com>
2022-02-06 22:23:08 +0300

    updated file

--- car.ts
+++ car.ts
@@ -1,13 +1,25 @@
+// import { Engine } from './engine';
+// import { Wheels } from './wheels';
+
+// export class Car{
+//   engine: Engine;
+//   wheels: Wheels;
+
+//   constructor(){
+//     this.engine = new Engine();
+//     this.wheels = new Wheels();
+//   }
+
+//   startEngine(){
+//     this.engine.start();
+//   }
+// }
 import { Engine } from './engine';
 import { Wheels } from './wheels';
 
 export class Car{
-  engine: Engine;
-  wheels: Wheels;
 
-  constructor(){
-    this.engine = new Engine();
-    this.wheels = new Wheels();
+  constructor(private engine: Engine,private wheels: Wheels){
   }
 
   startEngine(){
--- main.ts
+++ main.ts
@@ -1,9 +1,23 @@
+// import { Car } from './car';
+// import { Engine } from './engine';
+// import { Wheels } from './wheels';
+
+// function main(){
+//   let car = new Car();
+
+//   car.startEngine();
+// }
+
+// main();
 import { Car } from './car';
 import { Engine } from './engine';
 import { Wheels } from './wheels';
 
 function main(){
-  let car = new Car();
+
+  let engine = new Engine();
+  let wheels = new Wheels()
+  let car = new Car(engine,wheels);
 
   car.startEngine();
 }
